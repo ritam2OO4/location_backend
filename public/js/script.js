b5e31e1024bb927a47e006e2a4e1d0b57b1c680c
Ritam_devs <ritamdevs2004@gmail.com>
2024-09-10 12:28:46 +0530

    location/socket.io

---
+++ public/js/script.js
@@ -0,0 +1,41 @@
+const socket = io();
+
+if (navigator.geolocation) {
+    navigator.geolocation.watchPosition((position) => {
+        const { latitude, longitude } = position.coords;
+        socket.emit('set-location', { latitude, longitude })
+    }),
+        (error) => {
+            console.log(error)
+        },
+    {
+        enableHighAccuracy: true,
+        maximumAge: 0,
+        timeout: 5000,
+    }
+}
+
+const map = L.map('map').setView([0, 0], 16);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: 'Open Source Searching'
+}).addTo(map)
+
+const markers= {}
+socket.on('received-location', (data) => {
+    const { id, latitude, longitude } = data;
+    map.setView([latitude, longitude])
+    if (markers[id]) {
+        markers[id].setLatLng([latitude, longitude])
+    }
+    else {
+        markers[id] = L.marker([latitude, longitude]).addTo(map)
+    }
+})
+socket.on('user-disconnected', (id)=>{
+    if(markers[id]){
+        map.removeLayer(markers[id])
+        delete markers[id]
+    }
+})
+console.log(markers)
